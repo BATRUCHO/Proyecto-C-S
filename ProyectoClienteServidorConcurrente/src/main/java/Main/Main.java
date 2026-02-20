a221898907ce8c30fffe5f8f3c5e91dfc4ec1575
Bbatres@ <135394133+BATRUCHO@users.noreply.github.com>
2026-02-20 01:48:10 -0600

    Creacion de Proyecto
    1-Primera parte de creación del proyecto

--- ProyectoClienteServidorConcurrente/src/main/java/Main/Main.java
+++ ProyectoClienteServidorConcurrente/src/main/java/Main/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.proyectoclienteservidorconcurrente;
+package Main;
 
 /**
  *
  * @author braya
  */
-public class ProyectoClienteServidorConcurrente {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
