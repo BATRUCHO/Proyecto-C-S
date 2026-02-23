568c54ba4d2e9cb04b9433a7b4b7c60e185bac4b
Bbatres@ <135394133+BATRUCHO@users.noreply.github.com>
2026-02-23 10:42:57 -0600

    Traslado de ventanas

--- ProyectoClienteServidorConcurrente/src/main/java/Tabs/AsignacionFrame.java
+++ ProyectoClienteServidorConcurrente/src/main/java/Tabs/AsignacionFrame.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.interfaces;
+package Tabs;
 
 /**
  *
  * @author Asus Vivobook
  */
-public class ReportarIncidencia extends javax.swing.JFrame {
+public class AsignacionFrame extends javax.swing.JFrame {
 
     /**
-     * Creates new form ReportarIncidencia
+     * Creates new form AsignacionFrame
      */
-    public ReportarIncidencia() {
+    public AsignacionFrame() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class ReportarIncidencia extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ReportarIncidencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AsignacionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ReportarIncidencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AsignacionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ReportarIncidencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AsignacionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ReportarIncidencia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AsignacionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ReportarIncidencia().setVisible(true);
+                new AsignacionFrame().setVisible(true);
             }
         });
     }
